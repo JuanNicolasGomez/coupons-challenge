f37b8d1f422786697eee79dfb03b46ee54dbb57f
JuanNicolasGomez <nicolas.gm243@gmail.com>
2020-07-04 17:38:14 -0500

    [add] max amount calculate method and some unit tests

--- src/main/java/com/challenge/coupons/services/CouponServiceStub.java
+++ src/main/java/com/challenge/coupons/services/CouponServiceStub.java
@@ -2,6 +2,7 @@ package com.challenge.coupons.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +11,31 @@ public class CouponServiceStub implements CouponService {
 
     @Override
     public List<String> calculate(Map<String, Float> items, Float amount){
+        List<String> result = new ArrayList<>();
+        List<String> tempResult = new ArrayList<>();
+        float currentMaxAmount = 0;
+        float currentAmount = 0;
+        for (Map.Entry<String, Float> item : items.entrySet()) {
+            tempResult.clear();
+            tempResult.add(item.getKey());
+            System.out.println("Entry item: " + item.getKey() + "tempResult: "+ tempResult.toString());
+            currentAmount = item.getValue();
+            for (Map.Entry<String, Float> entry : items.entrySet()) {
+                if (item!=entry && currentAmount + entry.getValue() <= amount){
+                    currentAmount += entry.getValue();
+                    tempResult.add(entry.getKey());
 
-        return null;
+                }
+            }
+
+            if (currentAmount > currentMaxAmount){
+                currentMaxAmount = currentAmount;
+                result = new ArrayList<>(tempResult);
+                System.out.println("Result: " + (tempResult.toString()));
+            }
+            System.out.println("current " + currentAmount + ", maxCuurr: " + currentMaxAmount );
+        }
+        return result;
     }
+
 }
--- src/test/java/com/challenge/coupons/CouponsApplicationTests.java
+++ src/test/java/com/challenge/coupons/CouponsApplicationTests.java
@@ -1,17 +1,19 @@
 package com.challenge.coupons;
 
 import com.challenge.coupons.services.CouponService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class CouponsApplicationTests {
@@ -29,16 +31,38 @@ class CouponsApplicationTests {
 			items.put("MLA4",80f);
 			items.put("MLA5",90f);
 
-			List<String> expectedResult = new ArrayList<>();
-			expectedResult.add("MLA1");
-			expectedResult.add("MLA2");
-			expectedResult.add("MLA4");
-			expectedResult.add("MLA5");
 			List<String> result = couponService.calculate(items, 500f);
-			assertEquals(expectedResult,result);
+			assertThat(result, containsInAnyOrder("MLA1","MLA2","MLA4","MLA5"));
 		} catch (Exception ex) {
-			fail();
+			Assertions.fail();
 		}
 	}
+
+	@Test
+	public void calculateMaxValueProductsWithDecimals() {
+		try {
+			Map<String, Float> items = new HashMap<>();
+			items.put("MLA1",10.0f);
+			items.put("MLA2",8.50f);
+			items.put("MLA3",23.20f);
+			items.put("MLA4",12.0f);
+			items.put("MLA5",90.56f);
+			List<String> result = couponService.calculate(items, 100f);
+			assertThat(result, containsInAnyOrder("MLA2","MLA5"));
+		} catch (Exception ex) {
+			Assertions.fail();
+		}
+	}
+	@Test
+	public void calculateMaxValueProductsEmpty() {
+		try {
+			Map<String, Float> items = new HashMap<>();
+			List<String> result = couponService.calculate(items, 100f);
+			Assertions.assertEquals(result, new ArrayList<>());
+		} catch (Exception ex) {
+			Assertions.fail();
+		}
+	}
+
 
 }
