2f408a702040f880129f2cdaedaa530eae379632
JuanNicolasGomez <nicolas.gm243@gmail.com>
2020-07-07 21:33:44 -0500

    add method

--- src/main/java/com/challenge/coupons/respositories/ItemsRepositoryMock.java
+++ src/main/java/com/challenge/coupons/respositories/ItemsRepositoryMock.java
@@ -26,6 +26,7 @@ public class ItemsRepositoryMock implements ItemsRepository{
 
     @Override
     public Float getItemPrice(String itemId) {
-        return null;
+
+        return items.get(itemId);
     }
 }
